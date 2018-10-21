bfd752b67c741aa549c0f8cfc3b54ed39e436933
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-10-21 00:55:49 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -104,9 +104,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-		 
+     rer = programmer_hash[:alan_kay][:known_for]
 		 require 'pry'; binding.pry
-		 programmer_hash[:alan_kay][:known_for]
+		 
 
 
 end
